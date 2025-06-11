fc11bd1e30e0b9e4973c6e19b144a67856328cc5
Ranjit jana <jranjit367@gmail.com>
2025-06-11 18:06:38 +0530

    add a route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from  "express"
 
 const app = express();
 
+app.get('/', function(req,res){
+    res.send("hey")
+})
+
 app.listen(3000, ()=>{
     console.log('Server running on PORT 3000')
 })
