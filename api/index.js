30bcfdbca051d1a1db1fc23a982ca6483ba1fb13
Ranjit jana <jranjit367@gmail.com>
2025-06-11 21:52:09 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,23 +1,28 @@
-import express from  "express"
+import express from "express";
 import mongoose from "mongoose";
-import userRouter from './routes/user.route.js'
-import dotenv from 'dotenv'
+import userRouter from "./routes/user.route.js";
+import authRouter from './routes/auth.route.js'
+import dotenv from "dotenv";
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to MongoDb')
-}).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // ranjit9734
 // mongodb+srv://ranjit800:<db_password>@mern-real-estate.wnlinch.mongodb.net/?retryWrites=true&w=majority&appName=mern-real-estate
 
 const app = express();
 
-app.use('/api/user', userRouter)
+app.use(express.json())
 
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
 
-
-app.listen(3000, ()=>{
-    console.log('Server running on PORT 3000')
-})
+app.listen(3000, () => {
+  console.log("Server running on PORT 3000");
+});
