4786946d417729002d6568e8930d01544152a239
Ranjit jana <jranjit367@gmail.com>
2025-06-11 16:54:51 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/SignIn";
+import About from "./pages/About";
 
 const App = () => {
-  return <div className="text-2xl text-red-500">
-    app
-  </div>;
+  return (
+   
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/about" element={<About/>}/>
+   </Routes>
+
+  )
 };
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="text-4xl text-red-500">
+      Homme
+    </div>
+  )
+}
+
+export default Home
