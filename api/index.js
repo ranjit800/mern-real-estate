55f4482fc84ec8734d19df48676e88dee0bcfe3a
Ranjit jana <jranjit367@gmail.com>
2025-06-11 18:27:18 +0530

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from  "express"
+import mongoose from "mongoose";
+
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDb')
+}).catch((err)=>{
+    console.log(err)
+})
+
+// ranjit9734
+// mongodb+srv://ranjit800:<db_password>@mern-real-estate.wnlinch.mongodb.net/?retryWrites=true&w=majority&appName=mern-real-estate
 
 const app = express();
 
