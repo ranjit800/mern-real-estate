3cff6417b2f8207ccbb11c58d673db7bb17be66a
Ranjit jana <jranjit367@gmail.com>
2025-07-02 18:02:49 +0530

    update header search functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,34 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
 import logo from "../assets/Images/Logo.png";
 import { useSelector } from "react-redux";
 
+
 const Navbar = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
- 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-[#0f172a] shadow-2xl sticky top-0 z-50">
       <div className="flex justify-between items-center max-w-7xl mx-auto px-4 ">
@@ -17,9 +37,19 @@ const Navbar = () => {
           <img src={logo} alt="Luxora Realty Logo" className="h-20 object-contain" />
         </Link>
         {/* Search Bar */}
-        <form className="bg-[#f1f5f9] rounded-full px-3 py-2 items-center hidden sm:flex w-32 sm:w-64 md:w-80 focus-within:ring-2 focus-within:ring-[#d1b989] transition">
-          <input type="text" placeholder="Search..." className="bg-transparent outline-none w-full text-sm text-gray-700 placeholder:text-gray-500" />
-          <FaSearch className="text-gray-500 text-sm ml-2" />
+        <form
+          onSubmit={handleSubmit}
+          className="bg-[#f1f5f9] rounded-full px-3 py-2 items-center hidden sm:flex w-32 sm:w-64 md:w-80 focus-within:ring-2 focus-within:ring-[#d1b989] transition"
+        >
+          <input
+            onChange={(e) => setSearchTerm(e.target.value)}
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none w-full text-sm text-gray-700 placeholder:text-gray-500"
+          />
+          <button>
+            <FaSearch className="text-gray-500 text-sm ml-2" />
+          </button>
         </form>
 
         {/* Desktop Menu */}
