0064259b11de6934044cfbe1b7b39d85fac51678
Ranjit jana <jranjit367@gmail.com>
2025-07-15 03:03:15 +0530

    minor changes for deploy

--- api/index.js
+++ api/index.js
@@ -1,60 +1,44 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import dotenv from "dotenv";
-import cors from "cors"; // ✅ Import cors
-import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
-//deploy setup
+import cookieParser from "cookie-parser";
 import path from "path";
-
-// ✅ Load environment variables
 dotenv.config();
 
-// ✅ Connect to MongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDb");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
   });
 
-//deploy setup
 const __dirname = path.resolve();
 
 const app = express();
 
-// ✅ Set up CORS for your frontend origin
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-// ✅ Parse JSON bodies
 app.use(express.json());
 
 app.use(cookieParser());
 
-// Routes
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-//deploy setup
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
-
-
-// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -63,8 +47,4 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
-});
-
-app.listen(3000, () => {
-  console.log("Server running on PORT 3000");
 });
