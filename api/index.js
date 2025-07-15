9b4808bb01e8350861bf01562c1086d86e1c3576
Ranjit jana <jranjit367@gmail.com>
2025-07-15 16:35:23 +0530

    change 5

--- api/index.js
+++ api/index.js
@@ -83,8 +83,8 @@ app.use("/api/listing", listingRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
